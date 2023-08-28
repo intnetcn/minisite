72d76ccb3b445d299fa4d595a15ab32b586e1597
沐光於江湖 <140484436+intnetcn@users.noreply.github.com>
2023-08-28 14:35:36 +0800

    Add files via upload

---
+++ mc-admin/foot.php
@@ -0,0 +1,5 @@
+    </div>
+  </div>
+  <div id="footer"><div id="footer_box"> © <a href="https://8.hn.cn/">沐光於江湖</a> All Rights Reserved <span>v<?php echo $mc_config['version']; ?></span></div></div>
+</body>
+</html>
